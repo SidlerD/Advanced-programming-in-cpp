5040f294ca783b2f5ede060dbaaf82e358ef4b83
Dominik Sidler <Dominik.Sidler00@gmail.com>
2022-11-11 15:15:59 +0100

    Trivial benchmark complete

--- Exercise4/2_FindIf/Java/Benchmark.java
+++ Exercise4/2_FindIf/Java/Benchmark.java
@@ -3,13 +3,14 @@ import java.util.Vector;
 public class Benchmark {
     public static <T> long measure(Vector<T> vec, Matcher<T> matcher){
         long start = System.nanoTime();
-        FindIf.findIf(vec.iterator(), matcher);
+        T elem = FindIf.findIf(vec.iterator(), matcher);
         long end = System.nanoTime();
         
         long diff = end - start;
-        System.out.println("start: " + start + " nanoseconds");
-        System.out.println("end: " + end + " nanoseconds");
-        System.out.println("diff: " + diff + " nanoseconds");
+        // System.out.println("start: " + start + " nanoseconds");
+        // System.out.println("end: " + end + " nanoseconds");
+        System.out.println(elem + " found");
+        System.out.println("Time: " + diff / 1000000 + " miliseconds");
         return diff;
     }
 }
--- Exercise4/2_FindIf/Java/Main.java
+++ Exercise4/2_FindIf/Java/Main.java
@@ -2,14 +2,23 @@ import java.util.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        Vector<String> vec = new Vector<>();
-        for (int i = 0; i < 1000000; ++i) {
-            vec.add("elem " + i);
+        // Setup
+        int n = 10000000;
+        Vector<String> stringvec = new Vector<>();
+        for (int i = 0; i < n; ++i) {
+            stringvec.add("elem " + i);
         }
-        Matcher<String> stringMatcher = new Matcher<String>("elem 999999");
+        Matcher<String> stringMatcher = new Matcher<String>("elem " + (n-2));
+        
+        Vector<Integer> intvec = new Vector<>();
+        for (int i = 0; i < 10000000; ++i) {
+            intvec.add(i);
+        }
+        Matcher<Integer> intMatcher = new Matcher<Integer>(n-2);
+        
+        // Benchmark
+        Benchmark.measure(stringvec, stringMatcher);
+        Benchmark.measure(intvec, intMatcher);
 
-        String firstElem = FindIf.findIf(vec.iterator(), stringMatcher);
-        System.out.println(firstElem);
-        Benchmark.measure(vec, stringMatcher);
     }    
 }

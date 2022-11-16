e5b99495113db61671c898d1ac408528e6cdd92e
Dominik Sidler <Dominik.Sidler00@gmail.com>
2022-11-16 12:43:52 +0100

    Added stack

--- Exercise4/4_Merge/main.cpp
+++ Exercise4/4_Merge/main.cpp
@@ -2,9 +2,10 @@
 #include <vector>
 #include <set>
 #include <map>
+#include <stack>
 #include <numeric> // iota
 
-std::vector<int> vec_examples(){
+auto vec_examples(){
     std::vector<int> vec(20);
     std::iota(vec.begin(), vec.end(), 0);
 
@@ -14,7 +15,7 @@ std::vector<int> vec_examples(){
     return merge(vec, vec2);
 }
 
-std::set<int> set_examples(){
+auto set_examples(){
     std::set<int> s1({0, 2, 4, 6, 8});
     std::set<int> s2({1, 3, 5, 7, 9});
 
@@ -29,6 +30,12 @@ auto map_examples(){
     m2["Lena"] = 17; m2["Mirjam"] = 43; m1["Nathalie"] = 734;
 
     return merge(m1, m2);
+}
+
+auto stack_examples(){
+    std::stack<std::string> s1; s1.push("hello"); s1.push("world"); s1.pop(); s1.push("la monde");
+    std::stack<std::string> s2; s2.push("other"); s2.push("stack");
+    return merge(s1, s2);   
 }
 
 int main(){

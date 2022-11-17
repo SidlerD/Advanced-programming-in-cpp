f182f048932d649c51099e79f7d849219c800dba
Dominik Sidler <Dominik.Sidler00@gmail.com>
2022-11-17 14:35:45 +0100

    temp

--- Exercise4/4_Merge/merge-containers.hh
+++ Exercise4/4_Merge/merge-containers.hh
@@ -5,6 +5,7 @@
 #include <iterator>
 #include <algorithm>
 #include <map>
+#include <stack>
 
 // https://devblogs.microsoft.com/oldnewthing/20190619-00/?p=102599
 template<typename C, typename T = typename C::value_type>
@@ -15,6 +16,16 @@ C merge(const C& cont1, const C& cont2){
     
     return m;
 }
+
+template <typename T>
+std::stack<T> merge(const std::stack<T>& cont1, const std::stack<T>& cont2){
+    std::stack<T> m(cont1); // Copy contents of container 1
+
+    std::copy(cont2.begin(), cont2.end(), std::inserter(m, m.end())); // Copy contents of container 2
+    
+    return m;
+}
+
 
 template <typename T>
 void display_merged(T container){

45497d269767fc2722788db9c4dc6494be4c8236
Dominik Sidler <Dominik.Sidler00@gmail.com>
2022-11-18 18:47:28 +0100

    Additional checks

--- Exercise4/3_Range/main.cpp
+++ Exercise4/3_Range/main.cpp
@@ -9,7 +9,21 @@ int main(){
     // for(;first < last; ++first){
     //     std::cout << *first << std::endl;
     // }
+    try {
+        for(auto i : range(10,20)) std::cout << i << std::endl;
+    }
+    catch(const std::exception& e){}
+    
+    try {
+        for(auto i: range(12, 12)) std::cout << i << std::endl;
+    }
+    catch(const std::exception& e){}
+    
+    try {
+        for(auto i: range(12, 11)) std::cout << i << std::endl;
+    }
+    catch(const std::exception& e){}
 
-    for(auto i : range(10,20)) std::cout << i << std::endl;
+
     return 0;
 }

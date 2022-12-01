d53e181524015758108d37a6f2a433d9769bb6f0
Dominik Sidler <Dominik.Sidler00@gmail.com>
2022-12-01 09:59:20 +0100

    Finished

--- Exercise5/3_Shapes/Rectangle.h
+++ Exercise5/3_Shapes/Rectangle.h
@@ -8,4 +8,6 @@ public:
     Rectangle(double width, double height): a(width), b(height){}
     virtual double area(){ return a * b; }
     virtual double circumference(){ return 2*a + 2*b; }
+    virtual double get_height(){ return b; }
+    virtual double get_width(){ return a; }
 };
--- Exercise5/3_Shapes/main.cpp
+++ Exercise5/3_Shapes/main.cpp
@@ -1,15 +1,27 @@
 #include "Square.h"
 #include "Rectangle.h"
 #include <iostream>
+#include <vector>
+
+std::vector<double> calc_middle(Rectangle rect){
+    return std::vector<double> ({rect.get_width()/2, rect.get_height()/2});
+}
 
 int main(){
     // Every Square is a rectangle, but not every rectangle is a square
-    Rectangle r(2,5);
-    Square s(5);
+    
+    Rectangle r(2,5); // Not semantically equivalent to any Square
+    Square s(5); // Semantically equal to Rectangle(5,5)
 
+    // Problem: Rectangle could expose a method like setWidth & setHeight, which Square can't support
+    // But if Square was parent of Rect, then Rect would be limited in it's functionality
     std::cout << "r.area = " << r.area() << "\n";
     std::cout << "s.area = " << s.area() << "\n";
 
+    auto rmid = calc_middle(r);
+    auto smid = calc_middle(s);
+    std::cout << "Middle of r = " << rmid[0] << ", " << rmid[1] << std::endl;
+    std::cout << "Middle of s = " << smid[0] << ", " << smid[1] << std::endl;
 
 
     return 0;

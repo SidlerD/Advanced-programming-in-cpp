31b86247f5ac9eae63ef297cb40ce36a89a15b29
Dominik Sidler <Dominik.Sidler00@gmail.com>
2022-12-04 14:09:23 +0100

    Simplified

--- Exercise6/2_Metaprogramming/fib.hh
+++ Exercise6/2_Metaprogramming/fib.hh
@@ -1,20 +1,15 @@
 #pragma once
 
 template <unsigned int N>
-struct fib_impl{
-    static const int res = fib_impl<N-1>::res + fib_impl<N-2>::res;
+struct fib{
+    static const int res = fib<N-1>::res + fib<N-2>::res;
 };
 
 template <>
-struct fib_impl<1>{
+struct fib<1>{
     static const int res = 1;
 };
 template <>
-struct fib_impl<0>{
+struct fib<0>{
     static const int res = 0;
-};
-
-template <unsigned int N>
-struct fib{
-    static const int res = fib_impl<N>::res;
 };
--- Exercise6/2_Metaprogramming/gcd.hh
+++ Exercise6/2_Metaprogramming/gcd.hh
@@ -1,18 +1,12 @@
 #pragma once
 
 template <int A, int B>
-struct _gcd{
-    static const int res = _gcd<B, A%B>::res;
-};
-
-template<int A>
-struct _gcd<A, 0>{
-    static const int res = A;
-};
-
-template <int A, int B>
 struct gcd{
-    static const int res = _gcd<A, B>::res;
+    static const int res = gcd<B, A%B>::res;
+};
+template<int A>
+struct gcd<A, 0>{
+    static const int res = A;
 };
 
 int gcdf(int a, int b){
